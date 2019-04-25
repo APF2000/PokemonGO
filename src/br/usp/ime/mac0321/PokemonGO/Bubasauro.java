020d212580e4d647e6705a863fbffe69f86ff3c0
mac0321 07 <mac0321.07@estigma.cec.ime.usp.br>
2019-04-25 12:01:23 -0300

    teste 3

--- src/br/usp/ime/mac0321/PokemonGO/Bubasauro.java
+++
@@ -1,6 +0,0 @@
-package br.usp.ime.mac0321.PokemonGO;
-
-public class Bubasauro {
-	final String nome = "Bubasauro";
-	final Move[] ataques= {new Move("teste0",10),new Move("teste1",10),new Move("teste2",10),new Move("teste3",10)};
-}
--- src/br/usp/ime/mac0321/PokemonGO/PokemonController.java
+++ src/br/usp/ime/mac0321/PokemonGO/PokemonController.java
@@ -6,53 +6,17 @@ public class PokemonController extends Controller{
 		
 		Treinador esponja = new Treinador("Bob esponja");
 		Treinador construtor = new Treinador("Bob, o construtor");
+		esponja.addPoke(new Pikachu);
+		esponja.addPoke(new Gyarados);
+		construtor.addPoke(new Bubasauro);
+		construtor.addPoke(new Charmander);
+		
+		System.out.println("vai, se não o Pires me mata...");
 		
 		
 		
-		
-		public Restart(long eventTime) {
-			super(eventTime);
-		}
-
-		public void action() {
-		
-			long tm = System.currentTimeMillis();
-
-			// Instead of hard-wiring, you could parse
-
-			// configuration information from a text
-
-			// file here:
-
-			rings = 5;
-
-			addEvent(new );
-
-			addEvent(new LightOn(tm + 1000));
-
-			addEvent(new LightOff(tm + 2000));
-
-			addEvent(new WaterOn(tm + 3000));
-
-			addEvent(new WaterOff(tm + 8000));
-
-			addEvent(new Bell(tm + 9000));
-
-			addEvent(new ThermostatDay(tm + 10000));
-
-			// Can even add a Restart object!
-
-			addEvent(new Restart(tm + 20000));
-
-		}
-
-		public String description() {
-
-			return "Restarting system";
-
-		}
-
 	}
+    
 	
 	public static void main(String[] args) {
 
@@ -61,4 +25,5 @@ public class PokemonController extends Controller{
 		poke.addEvent(poke.new Restart(tm));
 		poke.run();
 	}
+	
 }
