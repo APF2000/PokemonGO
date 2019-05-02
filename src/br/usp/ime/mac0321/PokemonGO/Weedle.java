ac64be16567fa63e4e347f464f49ffddc3357e79
APF2000 <arthurpfonseca@gmail.com>
2019-05-01 22:56:15 -0300

    Update_01/05/2019_22:56

---
+++ src/br/usp/ime/mac0321/PokemonGO/Weedle.java
@@ -0,0 +1,10 @@
+package br.usp.ime.mac0321.PokemonGO;
+
+public class Weedle extends Pokemon {
+	final String nome = "Squirtle";
+	final private Move[] ataques= {new Move("teste0",10),new Move("teste1",10), new Move("teste2",10),new Move("teste3",10)};
+	
+	public Weedle() {
+		super();
+	}
+}
