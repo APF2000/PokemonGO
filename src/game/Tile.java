e1e6c22cf09931a066a518300747916887a2db9f
APF2000 <arthurpfonseca@gmail.com>
2019-05-14 22:11:56 -0300

    Movimento circular nas col e lin + prob de 1/6 de um pedaco do mapa ser grama

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -2,17 +2,16 @@ package game;
 import java.util.Random;
 
 public class Tile {
-	private final boolean comum = false;
-	private final boolean gramado = true;
+	public static final boolean comum = false;
+	public static final boolean gramado = true;
 
-	private final int direita = 1; 
-	private final int esquerda = 2; 
-	private final int cima = 3; 
-	private final int baixo = 4;
+	public static final int direita = 1, cima = 2;
+	public static final int esquerda = -1, baixo = -2;
 
 	private boolean tipo;
-	private int linha_atual;
-	private int coluna_atual;
+	private static int atual_lin;
+	private static int atual_col;
+
 
 	public Tile() {
 		Random rand = new Random();
@@ -21,27 +20,65 @@ public class Tile {
 		// queremos que a probabilidade de ser gramado
 		// seja 1/6, e de ser normal de 5/6
 		int random = rand.nextInt(7);
-		for(int i = 0; i < 100; i++) {
-			random = rand.nextInt(7);
-			tipo = (random % 6 == 0);
-			System.out.println(random + "\n" + tipo);
-		}
+		random = rand.nextInt(7);
+		tipo = (random % 6 == 0);
+		System.out.println("random % 6 == 0 " + (random%6==0));
+	}
+	
+	public boolean tipo() {
+		return tipo;
 	}
 
-	public Tile[][] criaMapa(int linhas, int colunas) {
+	public static Tile[][] criaMapa(int linhas, int colunas) {
 		Tile[][] mapa = new Tile[linhas][colunas]; 
 		for(int i = 0; i < linhas; i++) {
 			for(int j = 0; j < colunas; j++) {
 				mapa[i][j] = new Tile();
 			}
 		}
-		
+
+
 		Random rand = new Random();
-		linha_atual = rand.nextInt(linhas);
-		coluna_atual = rand.nextInt(colunas);
-		
+		atual_lin = rand.nextInt(linhas);
+		atual_col = rand.nextInt(colunas);
+
 		return mapa;
 	}
 
-	
+	public static void anda(int direcao, int linhas, int colunas) {
+		System.out.println("\nMovido da posicao (" + atual_lin + ", " 
+				+ atual_col + ")");
+
+		if(direcao == direita || direcao == esquerda) {
+			atual_lin += direcao + linhas;
+			atual_lin %= linhas;
+		}
+		else if(direcao == cima || direcao == baixo){
+			atual_col += (direcao / 2) + colunas;
+			atual_col %= colunas;
+		}
+
+		System.out.println("para (" + atual_lin + ", " 
+				+ atual_col + ")");
+	}	
+
+	/*public static void main(String[] args) {
+		Tile[][] mapa;
+		mapa = criaMapa(30, 30);
+		for(int i = 0; i < 30; i++) {
+			for(int j = 0; j < 30; j++) {
+				Tile.anda(direita, 30, 30);
+				System.out.print("Onde há ");
+				
+				if(mapa[i][j].tipo() == gramado) {
+					System.out.println("grama");
+				}
+				else if(mapa[i][j].tipo() == comum) {
+					System.out.println("chão");
+				}
+			}
+			Tile.anda(baixo, 30, 30);
+		}
+	}
+	*/
 }
